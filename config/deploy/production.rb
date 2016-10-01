cac217d342f5a1a0c6efed21d4f4543f57908fdf
AkPeter1 <akkpeter@gmail.com>
2016-10-01 12:20:14 +0300

    first commit 5

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,4 +5,4 @@ set :ssh_options, { forward_agent: true, port: 22 }
 set :branch, 'master'
 set :deploy_to, '/home/deploy'
 
-server '46.101.131.48', user: 'deploy', roles: %w{web app db}
+server '46.101.169.65', user: 'deploy', roles: %w{web app db}
