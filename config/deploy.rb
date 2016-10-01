11b2ae6a567592eb885297fa91275454daf5abf3
AkPeter1 <akkpeter@gmail.com>
2016-10-02 00:44:42 +0300

    first commit 13

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,23 +11,22 @@ set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/sys
 set :bundle_binstubs, nil
 set :passenger_restart_with_touch, true
 
-desc 'Runs rake db:seed'
-task :seed => [:set_rails_env] do
-  on primary fetch(:migration_role) do
-    within release_path do
-      with rails_env: fetch(:rails_env) do
-        execute :rake, "db:seed"
-      end
-    end
-  end
-end
-
 namespace :deploy do
   desc 'Restart application'
-
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  desc 'Runs rake db:seed'
+  task :seed => [:set_rails_env] do
+    on primary fetch(:migration_role) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, "db:seed"
+        end
+      end
     end
   end
 
