6296f04fce71739d960bc6eb4fd6dfea1436cda3
AkPeter1 <akkpeter@gmail.com>
2016-10-02 12:15:18 +0300

    first commit 14

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  # devise_for :users
+
+  devise_for :users, :skip => [:registrations]
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users/:id' => 'devise/registrations#update', :as => 'user_registration'
+  end
+
   get 'main/index'
   root 'main#index'
 
